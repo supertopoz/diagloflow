74b26512751b958f75a486c2fa0f9fe585869a61
supertopoz <deepsleepsounds@gmail.com>
2022-01-24 12:28:04 +0900

    add new pool

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var pgtools = require("pgtools");
 require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
@@ -21,24 +20,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/db', dbRouter)
+app.use('/database', dbRouter)
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.use(function(req, res, next) {
+//   console.log("ERROR FUNCT")
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   console.log("ERROR!")
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
--- routes/db.js
+++ routes/db.js
@@ -4,25 +4,77 @@ var router = express.Router();
 
 const db_url = process.env.DATABASE_URL
 
-const pool = new Pool({
-    connectionString: db_url,
-    ssl: {
-        rejectUnauthorized: false
+
+const execute = async (query, client) => {
+    try {
+        await client.query(query);  // sends queries
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    } finally {
+        await client.end();         // closes connection
     }
-})
+};
+
+const createPool = () => {
+
+    try {
+        pool = new Pool({
+            connectionString: db_url,
+            ssl: {
+                rejectUnauthorized: false
+            }
+            // ssl: false
+        })
+        return pool
+    } catch (e) {
+
+        console.log("POOL ERROR!", e)
+        return
+    }
+
+}
+
+
+
 
 /* GET users listing. */
-router.get('/db', async function (req, res, next) {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM test_table');
-        const results = { 'results': (result) ? result.rows : null};
-        res.render('pages/db', results );
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-});
+router.get('/', async function (req, res, next) {
 
+    let pool = null
+    let client = null;
+    // try {
+    //     const pool1 = createPool();
+    //     client = await pool1.connect();
+    //     const result = await client.query('SELECT * FROM test_table');
+    //     const results = { 'results': (result) ? result.rows : null};
+    //     // res.render('pages/db', results );
+    //     res.status(200).send(results)
+    //     client.release();
+    //
+    // } catch (err) {
+        const pool2 = createPool();
+        const client2 = await pool2.connect();
+        const text = `
+    CREATE TABLE IF NOT EXISTS "test_table" (
+	    "id" SERIAL,
+	    "name" VARCHAR(100) NOT NULL,
+	    "role" VARCHAR(15) NOT NULL,
+	    PRIMARY KEY ("id")
+    );`;
+
+        execute(text, client2).then(result => {
+            if (result) {
+                console.log(result)
+                console.log('Table created');
+            }
+            client2.release();
+            res.status(200).send(result);
+        });
+
+        // console.error(err);
+        // res.send("Error " + err);
+     // }
+});
 module.exports = router;
