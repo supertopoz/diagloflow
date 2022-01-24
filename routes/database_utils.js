e93aed1040975e9129e3e7dcc0a95e429b90f5a6
supertopoz <deepsleepsounds@gmail.com>
2022-01-24 13:54:14 +0900

    add new pool

---
+++ routes/database_utils.js
@@ -0,0 +1,63 @@
+const {Pool} = require("pg");
+
+const db_url = process.env.DATABASE_URL
+
+module.exports.createPool = () => {
+    try {
+        pool = new Pool({
+            connectionString: db_url,
+            ssl: {
+                rejectUnauthorized: false
+            }
+            // ssl: false
+        })
+        return pool
+    } catch (e) {
+
+        console.log("POOL ERROR!", e)
+        return
+    }
+}
+
+module.exports.checkTableExistence = async (client, tableName) => {
+    console.log(tableName)
+    try {
+        const table = await client.query(`
+            SELECT EXISTS (
+                SELECT FROM 
+                    pg_tables
+                WHERE 
+                schemaname = 'public' AND 
+                tablename  = '${ tableName }'
+            );
+    `)
+        if(table.rows[0].exists === true) {
+            return {error: false, message : "Table found"}
+        } else {
+            return {error: true, message: "Table not found"}
+        }
+    } catch (e) {
+        return {error: true, message: e}
+    } finally {
+
+    }
+}
+
+const createTable = async (client, tableName) => {
+
+    const text = `
+    CREATE TABLE IF NOT EXISTS ${tableName} (
+        "id" SERIAL,
+        "user_id" VARCHAR(100) NOT NULL,
+        "convo_id" VARCHAR(150) NOT NULL,
+        PRIMARY KEY ("id")
+    );`
+    try {
+        const table = await client.query(text)
+        return {error:false, message: `Created table: ${tableName}`}
+    } catch (e) {
+        return {error:true, message: "Could not create table", errorMessage: e}
+    } finally {
+        client.end()
+    }
+}
--- routes/db.js
+++ routes/db.js
@@ -1,80 +1,31 @@
 var express = require('express');
 const {Pool} = require("pg");
+const { createPool, checkTableExistence } = require("./database_utils")
 var router = express.Router();
-
-const db_url = process.env.DATABASE_URL
-
-
-const execute = async (query, client) => {
-    try {
-        await client.query(query);  // sends queries
-        return true;
-    } catch (error) {
-        console.error(error.stack);
-        return false;
-    } finally {
-        await client.end();         // closes connection
-    }
-};
-
-const createPool = () => {
-
-    try {
-        pool = new Pool({
-            connectionString: db_url,
-            ssl: {
-                rejectUnauthorized: false
-            }
-            // ssl: false
-        })
-        return pool
-    } catch (e) {
-
-        console.log("POOL ERROR!", e)
-        return
-    }
-
-}
-
-
-
 
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
 
-    let pool = null
-    let client = null;
-    // try {
-    //     const pool1 = createPool();
-    //     client = await pool1.connect();
-    //     const result = await client.query('SELECT * FROM test_table');
-    //     const results = { 'results': (result) ? result.rows : null};
-    //     // res.render('pages/db', results );
-    //     res.status(200).send(results)
-    //     client.release();
-    //
-    // } catch (err) {
-        const pool2 = createPool();
-        const client2 = await pool2.connect();
-        const text = `
-    CREATE TABLE IF NOT EXISTS "test_table" (
-	    "id" SERIAL,
-	    "name" VARCHAR(100) NOT NULL,
-	    "role" VARCHAR(15) NOT NULL,
-	    PRIMARY KEY ("id")
-    );`;
 
-        execute(text, client2).then(result => {
-            if (result) {
-                console.log(result)
-                console.log('Table created');
-            }
-            client2.release();
-            res.status(200).send(result);
-        });
-
-        // console.error(err);
-        // res.send("Error " + err);
-     // }
+    //Check if the table exists.
+    const pool = createPool();
+    const client = await pool.connect();
+    const tableName = req.query.table
+    const tableExists = await checkTableExistence(client, tableName)
+    if (tableExists.error) {
+        //Do some work creating the table
+        const table = await createTable(client, tableName)
+        if(table.error){
+            res.status(404).send({error: true, message: "Table not found and not created"});
+        } else {
+            res.status(200).send({error: false, message:table.message})
+        }
+        client.end()
+        return
+    } else {
+        res.status(200).send(tableExists);
+        client.end()
+        return
+    }
 });
 module.exports = router;
