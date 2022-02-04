db25bd2ee88ec63284836723eb4db8a1263ea4cf
supertopoz <deepsleepsounds@gmail.com>
2022-02-04 14:42:35 +0900

    full service

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var logger = require('morgan');
 
@@ -18,6 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
